041f3ce29d1708e4d1404b11a05f661e24fd59d9
Hernan Carvente <carvente.hernan@gmail.com>
2020-10-05 21:12:06 -0400

    uploading loops files

---
+++ class_scripts/bootcamp_scripts/loops.py
@@ -0,0 +1,58 @@
+# the syntax 
+
+# for loop
+# numbers = [12, 8, 41]
+
+# for num in numbers:
+# 	print(num)
+
+#complicated case string
+
+# lower case strings
+# lower_list = ['here', 'are', 'my', 'lower', 'case', 'words']
+# # upper list 
+# upper_list = []
+
+# for i in lower_list:
+# 	upper_list.append(i.upper())
+
+
+#mixed data types
+# mixed = [23. 'word', 23.4, True]
+
+# data_types = []
+
+# for date in mixed:
+# 	data_types.append(type(data)
+# 		print(data_types)
+
+# my_string = 'ice cream'
+# vowels = ['a', 'e', 'i', 'o', 'u']
+
+# for char in my_string:
+# 	if char == '':
+# 		print(f'{char} is a space')
+# 	elif char in vowels:
+# 		print(f'{char} is a vowel')
+# 	else:
+# 		print(f'{char}. Nothing to see here!')	
+
+for i in range(100):
+	print(i + 1)
+
+# while loops
+
+count = 0
+
+while count < 5:
+	print(count)
+	count = count + 1
+
+
+print('done')
+
+
+
+
+
+
